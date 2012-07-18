f0ff8a4f95e52b6521c47bd94c24fe42742aa1cf
AJ Bahnken <avb.wkyhu@gmail.com>
2012-07-17 20:40:39 -0700

    initial commit

---
+++ instatornode/args.py
@@ -0,0 +1,73 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='''InstaTorNode -- Get a
+                                     Tor Node running on an EC2 instance in
+                                     one line.''')
+
+parser.add_argument('-v', '--version', action='version',
+                     version='InstaTorNode v0.1')
+
+parser.add_argument('--access-key-id', '-akid', action='store', dest='akid',
+                    help='''Where you must insert your AWS Access Key ID.
+                    (More info here: http://goo.gl/fCVGi)''',
+                    required=True)
+parser.add_argument('--secret-access-key', '-sak', action='store', dest='sak',
+                    help='''Where you must insert your AWS Secret Access Key.
+                    (More info here: http://goo.gl/fCVGi)''',
+                    required=True)
+
+parser.add_argument('--security-group', '-secgrp', action='store',
+                    dest='secgrp', help='''Name of the Security Group
+                    you want to use for the instance. Checks to see if
+                    it already exists, if it does not, creates it.
+                    (Defaults to 'tor-cloud-servers')
+                    ''')
+
+parser.add_argument('--keypair', '-kp', action='store', dest='keypair',
+                    help='''Trys to use the keypair selected, if it does not
+                    exist, creates it.  (Defaults to ~/.ssh/tor-cloud-servers.pem
+                    if nothing is inputted.)''')
+
+parser.add_argument('--user-data', '-ud', action='store', dest='userdata',
+                    help='''Auto-run a script when the instance starts. 
+                    (Default is None. You usually don't need to mess with
+                    this)''')
+
+
+bridge_type = parser.add_mutually_exclusive_group()
+bridge_type.add_argument('--private', '-prvt', action='store_true',
+                         dest='private', help='''Creates a Private Bridge.''')
+bridge_type.add_argument('--normal', '-nrml', action='store_true',
+                         dest='normal', help='''Creates a Normal Bridge. (Default)''')
+
+
+instance_type = parser.add_mutually_exclusive_group()
+instance_type.add_argument('--micro', action='store_true', dest='micro',
+                           help='''Micro Instance Type. (Default)''')
+instance_type.add_argument('--small', action='store_true', dest='small',
+                           help='''Small Instance Type.''')
+instance_type.add_argument('--hcpumedium', action='store_true', 
+                           dest='highmed', help='''High-CPU Medium Instance
+                           Type.''')
+instance_type.add_argument('--medium', action='store_true', dest='med',
+                           help='''Medium Instance Type.''')
+
+ami = parser.add_mutually_exclusive_group()
+ami.add_argument('--us-east-1', '-use1', action='store_true', dest='use1',
+                 help='''Virginia (Default)''')
+ami.add_argument('--us-west-1', '-usw1', action='store_true', dest='usw1',
+                 help='''North California''')
+ami.add_argument('--us-west-2', '-usw2', action='store_true', dest='usw2',
+                 help='''Oregon''')
+ami.add_argument('--eu-west-1', '-euw1', action='store_true', dest='euw1',
+                 help='''Ireland''')
+ami.add_argument('--ap-northeast-1', '-apne1', action='store_true', dest='apne1',
+                 help='''Tokyo''')
+ami.add_argument('--ap-southeast-1', '-apse1', action='store_true', dest='apse1',
+                 help='''Singapore''')
+ami.add_argument('--sa-east-1', '-sae1', action='store_true', dest='sae1',
+                 help='''Sao Paulo''')
+
+Args = parser.parse_args()
+
---
+++ instatornode/instatornode.py
@@ -0,0 +1,164 @@
+#   InstaTorNode
+#   Orginal Author: AJ Bahnken / aj@ajvb.me
+#   Maintained by The Cloak Project / thecloakproject.org
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import argparse
+from os import path
+import sys
+
+import boto
+import requests
+
+from args import Args
+
+###
+#   Functions
+###
+def launch_instance(ec2,
+                    instance_type,
+                    ami,
+                    group_name='tor-cloud-servers',
+                    key_name='tor-cloud-servers',
+                    key_extension='.pem',
+                    key_dir='~/.ssh',
+                    ssh_port=22,
+                    cidr='0.0.0.0/0',
+                    user_data=None,
+                    ssh_passwd=None):
+    '''
+    The main function of this application. Will look extremely
+    familiar to anyone who has read 'Python and AWS' by Mitch Garnatt.
+    '''
+    try:
+        key = ec2.get_all_key_pairs(keynames=[key_name])[0]
+    except ec2.ResponseError, e:
+        if e.code == 'InvalidKeyPair.NotFound' :
+            print 'Creating keypair: %s' % key_name
+            key = ec2.create_key_pair(key_name)
+            key.save(key_dir)
+        else:
+            raise
+
+    try:
+        group = ec2.get_all_security_groups(groupnames=[group_name])[0]
+    except ec2.ResponseError, e:
+        if e.code == 'InvalidGroup.NotFound':
+            print 'Creating Security Group: %s' % group_name
+            group = ec2.create_security_group(group_name,
+                                              'Security Group for Tor Nodes')
+        else:
+            raise
+
+    try:
+        group.authorize('https', ssh_port, ssh_port, cidr)
+    except ec2.ResponseError, e:
+        if e.code == 'InvalidPermission.Duplicate':
+            print 'Security Group: %s already authorized' % group_name
+        else:
+            raise
+
+    reservation = ec2.run_instances(ami,
+                                    key_name=key_name,
+                                    security_groups=[group_name],
+                                    instance_type=instance_type,
+                                    user_data=user_data)
+    instance = reservation.instances[0]
+
+    while instance.state != 'running' :
+        print '.'
+        time.sleep(5)
+        instance.update()
+
+    return instance
+
+def get_ami(Args):
+    '''
+    Gets the correct AMI.
+    '''
+    if Args.private:
+        if Args.use1:
+            return 'ami-8467bfed'
+        elif Args.usw1:
+            return 'ami-63207b26'
+        elif Args.usw2:
+            return 'ami-baf27e8a'
+        elif Args.euw1:
+            return 'ami-f5390281'
+        elif Args.apne1:
+            return 'ami-78e05079'
+        elif Args.apse1:
+            return 'ami-e63472b4'
+        elif Args.sae1:
+            return 'ami-c4a07ed9'
+        else:
+            return 'ami-8467bfed'
+    elif Args.normal:
+        if Args.use1:
+            return 'ami-4e6eb627'
+        elif Args.usw1:
+            return 'ami-892378cc'
+        elif Args.usw2:
+            return 'ami-24f27e14'
+        elif Args.euw1:
+            return 'ami-6d447f19'
+        elif Args.apne1:
+            return 'ami-78ff4f79'
+        elif Args.apse1:
+            return 'ami-e45016b6'
+        elif Args.sae1:
+            return 'ami-b4a07ea9'
+        else:
+            return 'ami-4e6eb627'
+    else:
+        return 'ami-4e6eb627'
+    
+
+def get_type(Args):
+    '''
+    Gets the Instance Type through checking out the Args.
+    '''
+    if Args.small:
+        return 'm1.small'
+    elif Args.highmed:
+        return 'c1.medium'
+    elif Args.med:
+        return 'm1.medium'
+    else:
+        return 't1.micro'
+
+
+if __name__ == '__main__':
+    try:
+        ec2 = boto.connect_ec2()
+    except boto.exception.NoAuthHandlerFound:    
+        pass
+    try:
+        ec2 = boto.connect_ec2(Args.akid, Args.sak)
+    except:
+        sys.exit(1)
+
+    instance_type = get_type(Args)
+    ami = get_ami(Args)
+
+    instance = launch_instance(ec2=ec2, 
+                               instance_type=instance_type,
+                               ami=ami)
+    if instance:
+        print "Your " + instance.instance_type + " instance is running!"
+        print "IP:", instance.ip_address
+        print "ID:", instance.id
+        print "Region:", instance.region
+        print "DNS name:", instance.dns_name
